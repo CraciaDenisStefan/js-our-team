a276aebebc5b6d48219ab2a0d5eb7ae5ca032391
CraciDenisStefan <crdenis13@gmail.com>
2023-05-31 16:34:57 +0200

    Bonus 1

---
+++ Bonus1/js/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let infoTeam =[
+    {
+        'name': 'Wayne Barnet',
+        'role': 'Founder & CEO',
+        'image': 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        'name': 'Angela Carol',
+        'role': 'Chief Editor',
+        'image': 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'walter-gordon-office-manager.jpg',
+    },
+    {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'scott-estrada-developer.jpg',
+    },
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara-ramos-graphic-designer.jpg',
+    },
+]
+
+let container = document.getElementById('container')
+
+for(let dati in infoTeam){
+    console.log(infoTeam[dati].name)
+    console.log(infoTeam[dati].role)
+    console.log(infoTeam[dati].image)
+
+    let teamOnPage = `
+    <div>${infoTeam[dati].name}</div>
+    <div>${infoTeam[dati].role}</div>
+    <img src="./img/${infoTeam[dati].image}">
+    `
+   
+    container.innerHTML+= teamOnPage
+}
---
+++ Bonus1/readme.md
@@ -0,0 +1,25 @@
+Problema : Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite:
+1 - Crea un arrai di oggetti 
+2 - Al interno di ogni oggetto creare
+	2.1 - name role e image
+	2.2 .Ad ogni nome diamo un valore 
+
+MILESTONE 1: 
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+1 - Creare un ciclo for in 
+2 - Prendiamo l'array infoTeam e la facciamo ciclare al interno di dati 
+3 - Creiamo un console.log infoTeam[dati] per ogni chiave in modo da vede in consol solo la proprieta .
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+1 - Creare un contenitore nel html
+2 - Creare un collegamento in js con il contenitore 
+3 - Creare una variabile con dentro i dati da inserire nel html
+4 - InnerText per visualizzare in pagina la variabile creata
+
+Bonus 1 :
+Trasformare la stringa foto in una immagine effettiva
+1 - Sostituisci il div con dentro la stringa del nome img con il tag img 
+2 - Inserisci il percorso + ${infoTeam[dati].image}
